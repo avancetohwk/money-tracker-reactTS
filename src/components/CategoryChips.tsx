a59cd6c3a25560562a0681568fae66b9cefc7de0
Darien A. Toh Wai Ken <darien@alfacom.com.my>
2021-07-06 14:31:03 +0800

    overflow issue

--- src/components/CategoryChips.tsx
+++ src/components/CategoryChips.tsx
@@ -6,25 +6,30 @@ export default class CategoryChips extends React.Component{
 
     render(){
         return(
-            <div style={{display:'flex',justifyContent:'space-evenly', overflow:'auto'}} >
+            <div style={{overflow:'none'}}>
+                <div style={{display:'flex',justifyContent:'space-evenly',overflowX:'scroll'}} >
                 {this.array.map(a => {     
                     console.log("Entered");                 
                     // Return the element. Also pass key     
                     return (
-                        <div style={{margin:'5px'}}  key={a.text}>
-                            <div className="chipsContainer">
-                            <input type="radio" className="btn-check" name="btnradio" id={a.text} autoComplete="off" ></input>
-                            <label className="btn btn-light rounded" htmlFor ={a.text}>{a.text}</label>
+                        // <div className="" style={{margin:'5px', minWidth: 0, flexGrow:1,width:'100%'}}  key={a.text}>
+                            <div className="chipsContainer" key={a.text}>
+                                <button className="btn btn-light rounded"> {a.text}</button>
+                            {/* <input type="radio" className="btn-check" name="btnradio" id={a.text} autoComplete="off" ></input>
+                            <label className="btn btn-light rounded" htmlFor ={a.text}>{a.text}</label> */}
                                 {/* <button style={{padding:'7px'}} className=" chips">{a.text}</button> */}
                                 {/* <div style={{padding:'7px'}} className=" chips">
                                     {a.text}
                                     <i className="fa fa-times-circle pointer" ></i>
                                 </div> */}
                             </div>
-                        </div>) 
+                        // </div>
+                        ) 
                 })}
 
             </div>
+            </div>
+            
         )
         
     }
